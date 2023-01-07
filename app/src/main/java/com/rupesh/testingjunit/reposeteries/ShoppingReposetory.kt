312ebcab904a169dc4a1fd3dfc7d12cf1a13e344
Rupesh kumar Singh <kumarrupesh907@gmail.com>
2023-01-07 07:41:51 +0530

    changes

--- app/src/main/java/com/rupesh/testingjunit/reposeteries/ShoppingReposetory.kt
+++ app/src/main/java/com/rupesh/testingjunit/reposeteries/ShoppingReposetory.kt
@@ -3,6 +3,7 @@ package com.rupesh.testingjunit.reposeteries
 import androidx.lifecycle.LiveData
 import com.rupesh.testingjunit.data.local.ShoppingItem
 import com.rupesh.testingjunit.data.remote.responses.ImageResponse
+import com.rupesh.testingjunit.other.Resource
 import retrofit2.Response
 
 interface ShoppingReposetory {
@@ -10,5 +11,5 @@ interface ShoppingReposetory {
     suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
     suspend fun observeAllShoppingItem():List<ShoppingItem>
     suspend fun observeTotalPrice(): List<Float>
-    suspend fun searchForImage(imageQueary: String): Response<ImageResponse >
+    suspend fun searchForImage(imageQueary: String): Resource<ImageResponse >
 }
